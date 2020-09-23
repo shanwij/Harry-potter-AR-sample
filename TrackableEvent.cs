3253aa784b513f9c9658d96599a490f680503d32
Lakshan Wijewardana <48085984+shanwij@users.noreply.github.com>
2020-09-23 12:17:16 +0530

    script add trackable

---
+++ TrackableEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TrackableEvent : DefaultTrackableEventHandler
+{
+
+    public UnityEvent onTrackingFound;
+    public UnityEvent onTrackingLost;
+    // Start is called before the first frame update
+    protected override void OnTrackingFound()
+    {
+       base.OnTrackingFound();
+       onTrackingFound.Invoke();
+    }
+
+    // Update is called once per frame
+    protected override void OnTrackingLost()
+    {
+      base.OnTrackingLost();
+      onTrackingLost.Invoke();
+    }
+}
